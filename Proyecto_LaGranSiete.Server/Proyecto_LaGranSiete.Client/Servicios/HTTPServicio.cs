9a265a88cf1bdd7ea4683e6c5e2d4cd5f5a34f98
Leandro Bertero <leandro.bertero@hotmail.com>
2024-10-01 01:47:58 -0300

    ejecucion con errores en proyecto server contectando con front - client

--- Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.Client/Servicios/HTTPServicio.cs
+++ Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.Client/Servicios/HTTPServicio.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Text;
 using System.Text.Json;
 
 namespace Proyecto_LaGranSiete.Client.Servicios
@@ -26,6 +27,26 @@ namespace Proyecto_LaGranSiete.Client.Servicios
             else
             {
                 return new HTTPRespuesta<T>(default, true, response);
+            }
+        }
+
+        public async Task<HTTPRespuesta<object>> Post<T>(string url, T entidad)
+        {
+            var enviarJson = JsonSerializer.Serialize(entidad);
+
+            var enviarContent = new StringContent(enviarJson,
+                                Encoding.UTF8,
+                                "application/json");
+
+            var response = await http.PostAsync(url, enviarContent);
+            if (response.IsSuccessStatusCode)
+            {
+                var respuesta = await DesSerializar<object>(response);
+                return new HTTPRespuesta<object>(respuesta, false, response);
+            }
+            else
+            {
+                return new HTTPRespuesta<object>(default, true, response);
             }
         }
 
--- Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.Client/Servicios/IHTTPServicio.cs
+++ Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.Client/Servicios/IHTTPServicio.cs
@@ -4,5 +4,6 @@ namespace Proyecto_LaGranSiete.Client.Servicios
     public interface IHTTPServicio
     {
         Task<HTTPRespuesta<T>> Get<T>(string url);
+        Task<HTTPRespuesta<object>> Post<T>(string url, T entidad);
     }
 }
